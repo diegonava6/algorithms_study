0b0b727b0ff1a7b027a560d58d560ceef163110e
Diego Nava <diegomorenonava@gmail.com>
2022-10-05 20:13:44 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Study progress on 'Academind' algorithms course
+
+## Resources
+- [Notes](http://simp.ly/p/9V7bh2)
+- 
---
+++ module1/index.js
@@ -0,0 +1,4 @@
+//* Write an algorithm that takes an array of numbers as input and
+//* calculates the sum of those numbers.
+//* Define the Time Complexity of that algorithm and determine
+//* what the lowest possible Time Complexity is for this problem
