2ba71706f57205b93a3964fcfb9f302bf9079d0d
Diego Nava <diegomorenonava@gmail.com>
2022-11-09 17:20:19 -0600

    finished exercise 1
    - function that sums an array of numbers

--- module1/index.js
+++ module1/index.js
@@ -2,3 +2,15 @@
 //* calculates the sum of those numbers.
 //* Define the Time Complexity of that algorithm and determine
 //* what the lowest possible Time Complexity is for this problem
+
+function sumOfArray(arr) {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    result = result + arr[i];
+  }
+
+  return result;
+}
+
+sumOfArray([1, 3, 6]);
